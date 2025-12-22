165954db72834f7ede59546f3df87f29061ba176
jongminchung <chungjm0711@gmail.com>
2025-12-22 17:34:20 +0900

    chore: example 경로 제거

--- examples/spring-boot-app-example/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-//plugins {
-//    id("io.github.jongminchung.openapi-spring")
-//}
-//
-//java {
-//    toolchain {
-//        languageVersion = JavaLanguageVersion.of(25)
-//    }
-//}
--- examples/spring-boot-app-example/src/integrationTest/java/SampleTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-class SampleTest {
-    @Test
-    void sampleTest() {
-        Assertions.assertTrue(true);
-    }
-}
